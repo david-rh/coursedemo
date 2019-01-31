1c22511b6a122f59c2f12e5faf929152bc7c764b
David Anderson <david.anderson@redhat.com>
2019-01-31 09:40:47 -0600

    Update install.sh

--- city-work-order/install.sh
+++ city-work-order/install.sh
@@ -2,6 +2,8 @@
 
 injected_dir=$1
 source /usr/local/s2i/install-common.sh
+
+echo "*****Running custom install ${injected_dir}"
 #install_deployments ${injected_dir}/injected-deployments.war
 #install_modules ${injected_dir}/modules
 configure_drivers ${injected_dir}/drivers.env
